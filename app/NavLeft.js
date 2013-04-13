10c7aaf10ff42df00e5493c1234eb038352880a4
guopeng <guopeng@192.168.1.104>
2013-04-14 00:09:22 +0800

    添加亚马逊菜单

--- app/NavLeft.js
+++ app/NavLeft.js
@@ -5,27 +5,70 @@
  */
  
  define(function(require,exports,module){
+ 	require('resources/css/bootstrap.css');
+ 	require('sdk/components/jquery.menu-aim.js');
  	var NavLeftView = Ambow.extend(Ambow.View,{
  		
- 		tpl:['<ul>',
- 				'<li><a idx="1" class="dd" href="javascript:void(0)">模块一</a></li>',
- 				'<li><a idx="2" href="javascript:void(0)">模块二</a></li>',
- 				'<li><a idx="3" href="javascript:void(0)">ListView</a></li>',
- 				'<li><a idx="4" href="javascript:void(0)">UnderScore Api</a></li>',
- 				'<li><a idx="5" href="javascript:void(0)">Grass Api</a></li>',
-
- 			'</ul>'].join(''),
+// 		tpl:['<ul>',
+// 				'<li><a idx="1" class="dd" href="javascript:void(0)">模块一</a></li>',
+// 				'<li><a idx="2" href="javascript:void(0)">模块二</a></li>',
+// 				'<li><a idx="3" href="javascript:void(0)">ListView</a></li>',
+// 				'<li><a idx="4" href="javascript:void(0)">UnderScore Api</a></li>',
+// 				'<li><a idx="5" href="javascript:void(0)">Grass Api</a></li>',
+//
+// 			'</ul>'].join(''),
+ 		
  		
  		events: {
- 			'click li a': 'onNavItemClicked' 
+ 			'click li a.sub-menu-item, a.maintainHover': 'onSubMenuItemClicked' 
  		},
  		
  		render: function(){
- 			this.$el.html(this.tpl);
+ 			this.$el.html(_.template(this.tpl,this.data));
  			$('#content-aside').html(this.$el);
+ 			var $menu = $(".dropdown-menu");
+ 			$menu.menuAim({
+	            activate: activateSubmenu,
+	            deactivate: deactivateSubmenu,
+	            exitMenu: function() {return true;},
+	            exit:deactivateSubmenu
+
+	        });
+	        function activateSubmenu(row) {
+	            var $row = $(row),
+	                submenuId = $row.data("submenuId"),
+	                $submenu = $("#" + submenuId),
+	                height = $menu.outerHeight(),
+	                width = $menu.outerWidth();
+	
+	            // Show the submenu
+	            $submenu.css({
+	                display: "block",
+	                top: -1,
+	                left: width - 3,  // main should overlay submenu
+	                width:width
+	                //height: height - 4  // padding for main dropdown's arrow
+	            });
+	
+	            // Keep the currently activated row's highlighted look
+	            $row.find("a").addClass("maintainHover");
+	        }
+
+	        function deactivateSubmenu(row) {
+	            var $row = $(row),
+	                submenuId = $row.data("submenuId"),
+	                $submenu = $("#" + submenuId);
+	
+	            // Hide the submenu and remove the row's highlighted look
+	            $submenu.css("display", "none");
+	            $row.find("a").removeClass("maintainHover");
+	        }
  			return this;
  		},
- 		
+ 		onSubMenuItemClicked: function(){
+ 			 $(".popover").css("display", "none");
+            $("a.maintainHover").removeClass("maintainHover");
+ 		},
  		onNavItemClicked: function(e){
  			var idx = e.target.getAttribute('idx');
  			switch(idx){
@@ -46,7 +89,77 @@
  					break;
  			}
  			this.trigger('nav_module'+idx+'_click');
- 		}
+ 		},
+ 		
+ 		tpl: ['<div class="nav-collapse collapse">',
+            	'<ul class="nav">',
+              		'<li class="active">',
+                		'<ul class="dropdown-menu" role="menu">',
+                			'<% _.each(menu,function(rec){ %>',	
+                			'<li data-submenu-id="<%=rec.id%>">',
+                        		'<a href="#"><%=rec.title%></a>',
+                        		'<div id="<%=rec.id%>" class="popover">',
+                           			'<h3 class="popover-title"><%=rec.title%></h3>',
+                            		'<div class="popover-content">',
+                            			'<ul>',
+                            				'<% _.each(rec.menu,function(r){ %>',
+                            				'<li><a class="sub-menu-item" href="#!/<%=r.link%>"> <%=r.title%></a><li>',
+                            				'<%})%>',
+                            			'</ul>',
+                            		'</div>',
+                            	'</div>',	
+                    		'</li>',
+                    		'<% }) %>',
+                		'</ul>',
+                	'</li>',
+                '</ul>',
+               '</div>'].join(''),
+         data:{
+         	menu:[
+         		{
+         			id:'menu-api',
+         			title:'API',
+         			link:'api/underScore/',
+         			menu:[
+         				{
+         					title:'underscore',
+         					link:'api/underScore/'
+         				},
+         				{
+         					title:'grass',
+         					link:'api/grass/'
+         				}
+         			]
+         		},
+         		{
+         			id:'menu-demo',
+         			title:'页面演示',
+         			link:'module1/Page1/',
+         			menu:[
+         				{
+         					title:'page1',
+         					link:'module1/Page1/'
+         				},
+         				{
+         					title:'page2',
+         					link:'module2/Page2/'
+         				}
+         			]
+         		},
+         		{
+         			id:'menu-cmp',
+         			title:'组件示例',
+         			link:'ListApi/',
+         			menu:[
+         				{
+         					title:'ListView',
+         					link:'ListApi/'
+         				}
+         			]
+         		}
+         	]	
+         }
+         
  	});
  	
  	return NavLeftView;
